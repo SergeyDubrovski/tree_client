e7085b370debc9c594ae8cf14b7148e7f0033ac3
SergeyDubrovski <sovelectron@gmail.com>
2022-06-21 09:10:28 +0300

    1

--- src/components/ControlPage.jsx
+++ src/components/ControlPage.jsx
@@ -12,9 +12,9 @@ export default function ControlPage(props) {
   const [delay, setDelay] = useState(true);
 
   setTimeout(() => {
-    setDelay(false)
+    setDelay(false);
   }, 3000);
- 
+
   const onClick = () => {
     setMonitor(!monitor);
     if (monitor) setVideo("block");
@@ -28,21 +28,22 @@ export default function ControlPage(props) {
     switchRelay({ r1: relay.r1, r2: relay.r2 });
   };
 
-  return ( 
+  return (
     <div className={classes.controlPage}>
-    
       <div className={classes.controlVideoBlock}>
         <div className={classes.controlVideo}>
-          <Camera1 className={classes.controlCamera} style={video} />
+          <Camera1 className={classes.controlCamera} height={100} width={200} style={video}  />
         </div>
-  {delay ? <h2>WAIT...</h2> :
-      <div className={classes.btnVideo}>
-          <button className={classes.btnVideoMini} onClick={onClick}>
-            <img className={classes.poligonVideo} src={Poligon} alt="" />
-          </button>
-        </div>
-      }
-        
+      {delay ? (
+          <h2>WAIT...</h2>
+        ) : (
+          <div className={classes.btnVideo}>
+            <button style={{marginLeft:'20px'}} className={classes.btnVideoMini} onClick={onClick}>
+              <img className={classes.poligonVideo} src={Poligon} alt="" />
+            </button>
+          </div>
+        )}
+     
       </div>
 
       <div className={classes.btnLightWater}>
--- src/components/techPage/Camera1.jsx
+++ src/components/techPage/Camera1.jsx
@@ -2,25 +2,38 @@ import React, { useRef, useEffect } from "react";
 import { loadPlayer } from "rtsp-relay/browser";
 
 const Camera1 = (props) => {
-  const canvas = useRef(HTMLCanvasElement);
+  const canvas = useRef(null);
 
   useEffect(() => {
+    
     if (!canvas.current) throw new Error("Ref is null");
-
+     console.log(canvas.current );
     loadPlayer({
-      url: "ws://91.215.176.34:7003/api/stream",
+      url: "ws://91.215.176.34:7010/api/stream",
       canvas: canvas.current,
+      
     });
-  }, []);
-
+     
+    }, []);
+    const onData = (e) => {
+      
+      const ctx = e.getContext('2d'); 
+     console.log(ctx   );
+    }  
+    
   return (
     <div>
-      <canvas
-        height={props.height}
-        style={{ display: props.style }}
+        <canvas
+      onClick={onData} 
+        height={100}
+        width= {100} 
+        style={{ display: props.style, marginTop:'100px'  }}
         ref={canvas}
       />
+     
     </div>
+    
+    
   );
 };
 export default Camera1;
