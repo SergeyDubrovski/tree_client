f045e19a6a95a073f3b1b525ffe3bc473ddad711
Dubrovskaya Oksana <oxanadu@yandex.ru>
2022-06-26 18:20:04 +0300

    css7

--- src/components/Roots.jsx
+++ src/components/Roots.jsx
@@ -7,6 +7,11 @@ import { picture } from "../picture/picture";
 import { useState } from "react";
 import RootPic from "./RootPic";
 import { useRef } from "react";
+import robot1 from '../picture/robot1.png'
+import robot2 from '../picture/robot2.png'
+import robot3 from '../picture/dino.png'
+import spruce from '../picture/spruce.png'
+
 let i = 0;
 export default function Roots() {
   const [pic, setPic] = useState(picture[0]);
@@ -29,6 +34,17 @@ const ref = useRef();
       <NavBar />
 
     <RootPic   pic={pic}/>
+    
+    <div className={classes.robots}>
+    <img className={classes.robot1}  src={robot1} alt="img" />
+    <img className={classes.robot2}  src={robot2} alt="img" />
+    <img className={classes.robot3}  src={robot3} alt="img" />
+    
+    </div>
+     <img className={classes.spruceTop} src={spruce} alt="img" />
+     
+      
+      
     </div>
   );
 }
