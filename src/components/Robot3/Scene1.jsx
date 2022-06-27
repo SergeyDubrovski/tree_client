c9f6e30dd06be0e8d000d242c4bcae5baad0402f
Dubrovskaya Oksana <oxanadu@yandex.ru>
2022-06-27 21:19:39 +0300

    css10

--- src/components/Robot3/Scene1.jsx
+++ src/components/Robot3/Scene1.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './scene.module.css';
 import NavBar from '../NavBar';
-import { loadPlayer } from "rtsp-relay/browser"; 
+import { loadPlayer } from "rtsp-relay/browser";
 import { Link } from 'react-router-dom'
 import { useEffect } from 'react';
 import { useRef } from 'react';
@@ -10,27 +10,28 @@ import flower from '../../picture/flowerandrobot.png'
 import switchpic from '../../picture/switch.png'
 
 export default function Scene1() {
-  
-  
+
+
   return (
 
     <div className={classes.scene1}>
 
-        <Link to="/scene/scene2">
-             <img className={classes.flower} src={flower} alt="img" />
-        </Link>
-       
-    <div className={classes.speed}>
-       
-        <div className={classes.rectangle}>
-            <h2>0 km/h</h2>
-        </div>
-         <img className={classes.switch} src={switchpic} alt="img" />
-    </div>
-        
+      
+        <img className={classes.flower} src={flower} alt="img" />
+      
 
-        
+      <div className={classes.speed}>
+
+        <div className={classes.rectangle}>
+          <h2>0 km/h</h2>
+        </div>
+        <Link to="/scene/scene2">
+        <img className={classes.switch} src={switchpic} alt="img" />
+        </Link>
+      </div>
+
+
+
     </div>
   )
 }
- 
