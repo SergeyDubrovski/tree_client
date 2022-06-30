12fb225311ce5338c7298664071cce78aee3b7da
Dubrovskaya Oksana <oxanadu@yandex.ru>
2022-06-30 15:54:54 +0300

    css15

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import classes from "./pages.module.css";
+import flowerandrobot6 from "../picture/flowerandrobot6.png";
 
 export default function Help() {
   return (
-    <div>Help</div>
+    <div className={classes.help}>
+      <div className={classes.helpH}>Help the robot raise a dinosaur!</div>
+      <img className={classes.flowerandrobot6} src={flowerandrobot6} alt="" />
+      
+    </div>
   )
 }
